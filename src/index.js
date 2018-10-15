09ebed9949af53e0689cb332cd6bd6a7b3139407
Ealden Esto E. Escañan <ealden@escanan.com>
2018-10-15 12:08:21 +0800

    Add entry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
+import PouchDB from 'pouchdb'
+
+const db = new PouchDB('entries')
+const remoteCouch = false
 
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello, PouchDB! (+ Vue)'
+    name: '',
+    description: ''
+  },
+  methods: {
+    addEntry: function() {
+      let entry = {
+        _id: new Date().toISOString(),
+        name: this.name,
+        description: this.description
+      }
+
+      db.put(entry, function callback(err, result) {
+        if (!err) {
+          console.log('Successfull added an entry!')
+        }
+      })
+    }
   }
 })
